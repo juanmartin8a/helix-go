9a06792cd240697ebe592c7239493f4f020b79d4
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2025-07-30 10:24:02 -0500

    update query.hx

--- example/helixdb-cfg/queries.hx
+++ example/helixdb-cfg/queries.hx
@@ -1,7 +1,35 @@
 QUERY create_user(name: String, age: U32, email: String, now: I32) =>
-    user <- AddN<User>({name: name, age: age, email: email, created_at: now, updated_at: now})
-    RETURN user
+    newUser <- AddN<User>({name: name, age: age, email: email, created_at: now, updated_at: now})
+    RETURN newUser 
+
+QUERY create_users(users: [{name: String, age: U32, email: String, now: I32}]) =>
+    FOR { name, age, email, now } IN users {
+        AddN<User>({name: name, age: age, email: email, created_at: now, updated_at: now})
+    }
+    RETURN "Success" 
+
+QUERY update_user(id: ID, name: String, age: U32, email: String) =>
+    updatedUser <- N<User>(id)::UPDATE({name: name, age: age, email: email})
+    RETURN updatedUser 
 
 QUERY get_users() =>
     users <- N<User>
     RETURN users 
+
+QUERY get_user_by_id(id: ID) =>
+    user <- N<User>(id)
+    RETURN user 
+
+QUERY follow(followerId: ID, followedId: ID) =>
+    follower <- N<User>(followerId)
+    followed <- N<User>(followedId)
+    AddE<Follows>::From(follower)::To(followed)
+    RETURN "Success" 
+
+QUERY followers(id: ID) =>
+    followers <- N<User>(id)::In<Follows>
+    RETURN followers 
+
+QUERY following(id: ID) =>
+    following <- N<User>(id)::Out<Follows>
+    RETURN following 
--- example/helixdb-cfg/schema.hx
+++ example/helixdb-cfg/schema.hx
@@ -6,24 +6,10 @@ N::User {
     updated_at: I32,
 }
 
-N::Post {
-    content: String,
-    created_at: I32,
-    updated_at: I32,
-}
-
 E::Follows {
     From: User,
     To: User,
     Properties: {
         since: I32,
-    }
-}
-
-E::Created {
-    From: User,
-    To: Post,
-    Properties: {
-        created_at: I32,
     }
 }
